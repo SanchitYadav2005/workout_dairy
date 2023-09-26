238020cf0d9a747b4ba6bae6a60ad81adf0b1a73
unknown <sanchit2005yudavanshi@gmail.com>
2023-09-26 21:17:27 +0530

    added a middleware to get the path and method of the request '

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,12 @@ const express = require("express");
 
 const app = express();
 
+//middleware for logging out the path, and request method
 
+app.use((req,res,next)=>{
+    console.log(req.path, req.method);
+    next();
+})
 app.get('/', (req,res)=>{
     res.json({mssg: "welcome to the site!!"})
 })
