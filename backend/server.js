20ddecd8f82d72b1f48ee4877c4c814c08d38bf8
unknown <sanchit2005yudavanshi@gmail.com>
2023-09-26 21:50:04 +0530

    addes express.json and seted routes file

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,17 @@
 const express = require("express");
 
 const app = express();
+const workoutsRoutes = require('./routes/workouts');
 
+
+app.use(express.json())
 //middleware for logging out the path, and request method
-
 app.use((req,res,next)=>{
     console.log(req.path, req.method);
     next();
 })
-app.get('/', (req,res)=>{
-    res.json({mssg: "welcome to the site!!"})
-})
+//routes.
+app.use("/api/workouts",workoutsRoutes);
 
 
 app.listen(4000, ()=>{
