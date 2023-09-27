26af59a31fbf1067dfb8db3b420424daff0ff825
unknown <sanchit2005yudavanshi@gmail.com>
2023-09-27 21:28:18 +0530

    added workout controllers only delete and update is left

---
+++ backend/controllers/workoutController.js
@@ -0,0 +1,46 @@
+const Workout = require("../models/Workouts");
+
+// get all workouts.
+
+const getWorkouts = async (req,res)=>{
+    const workouts = await Workout.find({}).sort({createdAt: -1})
+    res.status(200).json(workouts)
+}
+
+// get single workout
+
+const getWorkout = async (req,res)=>{
+    const {id} = req.params;
+    const workout = await Workout.findOne(id)
+
+    if(!workout){
+        res.status(404).json({messg: "no such workout found"})
+    }
+
+    res.status(200).json(workout)
+}
+
+// create workout 
+
+const createWorkout = async (req,res)=>{
+    try{
+        const {title,reps,load} = req.body;
+        const workout = await Workout.create({title,reps,load})
+        res.status(200).json(workout)
+    }
+    catch(error){
+        res.status(400).json({error:error.message})
+    }
+}
+
+// update 
+
+
+// delete
+
+
+module.exports = {
+    getWorkouts,
+    getWorkout,
+    createWorkout
+}
