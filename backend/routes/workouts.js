114688f5edf1f5c1924f560938123f6d4fb479ee
unknown <sanchit2005yudavanshi@gmail.com>
2023-09-27 21:29:09 +0530

    added the controllers

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,31 +1,25 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router();
-
+const {
+  getWorkout,
+  getWorkouts,
+  createWorkout,
+} = require("../controllers/workoutController");
 
 //get workouts
-router.get('/',(req,res)=>{
-    res.json({mssg:"get all the workouts"})
-})
+router.get("/", getWorkouts);
 
 // get single workout.
-router.get('/:id',(req,res)=>{
-    res.json({mssg:"get a single workout"})
-})
+router.get("/:id", getWorkout);
 
 // Post a new workout
-router.post("/", (req,res)=>{
-    res.json({mssg:"create a new workout"})
-})
+router.post("/",createWorkout);
 //DELETE a workout
-router.delete("/:id",(req,res)=>{
-    res.json({mssg:"delete a workout"})
-})
+router.delete("/:id", (req, res) => {
+  res.json({ mssg: "delete a workout" });
+});
 
 //UPDATE  a workout
-router.patch("/:id",(req,res)=>{
-    res.json({mssg:"update a workout"})
-})
-
-
+router.patch("/:id", async (req, res) => {});
 
 module.exports = router;
