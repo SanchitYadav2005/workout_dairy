c5c16cf849586ac48496b0320af546ad9099f0d4
unknown <sanchit2005yudavanshi@gmail.com>
2023-09-28 21:56:29 +0530

    added workoutdetails component in our home page

--- forntend/src/pages/Home.jsx
+++ forntend/src/pages/Home.jsx
@@ -1,7 +1,29 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+//components
+import WorkOutDetails from "../components/WorkoutDetails";
+
 function Home() {
+  const [workouts, setWorkouts] = useState(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      const res = await axios("/api/workouts");
+      const json = await res.json();
+      if (res.ok) {
+        setWorkouts(json);
+      }
+    };
+    getData();
+  }, []);
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home">
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkOutDetails workout={workout} key={workout._id} />
+          ))}
+      </div>
     </div>
   );
 }
