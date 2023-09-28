e1a1fb5aff9e39e6cb78984ecfebb90ee0d8ebe0
unknown <sanchit2005yudavanshi@gmail.com>
2023-09-28 21:30:02 +0530

    added some basic styles and added pages and component to the parent component

--- forntend/src/App.jsx
+++ forntend/src/App.jsx
@@ -1,15 +1,21 @@
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+
+//pages & components.
+
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
-
-
   return (
     <>
-      <div>
-        
+      <Navbar />
+      <div className="pages">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
