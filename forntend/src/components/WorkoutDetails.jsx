861812718f056267d221bbf7ead4bad8aee02e58
unknown <sanchit2005yudavanshi@gmail.com>
2023-10-04 09:02:18 +0530

    added post request to send the data to the database and added delete request to delete a workout

--- forntend/src/components/WorkoutDetails.jsx
+++ forntend/src/components/WorkoutDetails.jsx
@@ -1,4 +1,18 @@
+import useWorkoutsContext from "../hooks/useWorkoutsContext"
+
 function WorkOutDetails({ workout }) {
+  const { dispatch } = useWorkoutsContext()
+
+  const handleClick = async () => {
+    const response = await fetch('/api/workouts/' + workout._id, {
+      method: 'DELETE'
+    })
+    const json = await response.json()
+
+    if (response.ok) {
+      dispatch({type: 'DELETE_WORKOUT', payload: json})
+    }
+  }
   return (
     <div className="workout-details">
       <h4>{workout.title}</h4>
--- forntend/src/components/WorkoutForm.jsx
+++ forntend/src/components/WorkoutForm.jsx
@@ -1,7 +1,61 @@
-function WorkoutForm(){
-    return(
-        <h1>workoutform</h1>
-    )
+import { useState } from "react";
+import axios from "axios";
+import useWorkoutsContext from "../hooks/useWorkoutsContext";
+
+function WorkoutForm() {
+  const {dispatch} = useWorkoutsContext();
+  const [title, setTitle] = useState("");
+  const [reps, setReps] = useState("");
+  const [load, setLoad] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+    
+    const workout = {title, load, reps}
+    const response = await axios.post('/api/workouts', {
+        body: JSON.stringify(workout),
+        headers:{
+            'Content-type' : 'application/json'
+        }
+    })
+    const json = await response.json();
+    if(!response.ok){
+        setError(json.error);
+        console.log(error)
+    }else if(response.ok){
+        setTitle("")
+        setLoad("")
+        setReps("")
+        setError(null)
+        console.log("new workout added!", json)
+        dispatch({type: 'CREATE_WORKOUT', payload: json})
+    }
+  }
+  return (
+    <form className="create" onSubmit={handleSubmit}>
+      <h3>Add new workout</h3>
+
+      <label> Exercise Title:</label>
+      <input
+        type="text"
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+      />
+      <label> Load (in kg):</label>
+      <input
+        type="number"
+        onChange={(e) => setLoad(e.target.value)}
+        value={load}
+      />
+      <label> Reps:</label>
+      <input
+        type="number"
+        onChange={(e) => setReps(e.target.value)}
+        value={reps}
+      />
+    </form>
+  );
 }
 
 export default WorkoutForm;
