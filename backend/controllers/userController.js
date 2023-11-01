6a5475a91ed9f2d8ecf392e6e012e95d0bc09d9c
unknown <sanchit2005yudavanshi@gmail.com>
2023-11-01 09:34:50 +0530

    created basic controller for user

---
+++ backend/controllers/userController.js
@@ -0,0 +1,13 @@
+
+
+// login user 
+
+module.exports.loginUser = async (req,res) =>{
+    res.json({msg: "login user"})
+}
+
+//sign up user
+
+module.exports.signUpUser = async (req,res) =>{
+    res.json({msg: "signUp user"})
+}
