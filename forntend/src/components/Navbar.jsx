39e3caa87edefab49efd472b915ee72ac60e363b
unknown <sanchit2005yudavanshi@gmail.com>
2023-11-03 08:57:34 +0530

    added routes

--- forntend/src/components/Navbar.jsx
+++ forntend/src/components/Navbar.jsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
 
 function Navbar() {
-
   return (
     <header>
       <div className="container">
         <Link to="/">
           <h1>Workout diry</h1>
         </Link>
+        <nav>
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Sign Up</Link>
+        </nav>
       </div>
     </header>
   );
--- forntend/src/hooks/useAuthContext.js
+++ forntend/src/hooks/useAuthContext.js
@@ -1,14 +1,14 @@
-import { AuthContext } from "../contexts/AuthContext"
-import { useContext } from "react"
+import { AuthContext } from "../contexts/AuthContext";
+import { useContext } from "react";
 
 const useAuthContext = () => {
-  const context = useContext(AuthContext)
+  const context = useContext(AuthContext);
 
-  if(!context) {
-    throw Error('AuthContext must be used inside an AuthContextProvider')
+  if (!context) {
+    throw Error("AuthContext must be used inside an AuthContextProvider");
   }
 
-  return context
-}
+  return context;
+};
 
 export default useAuthContext;
