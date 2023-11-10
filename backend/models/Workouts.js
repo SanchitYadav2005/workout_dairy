d84d2ace4313ef2411ea98313e90b084658d965e
unknown <sanchit2005yudavanshi@gmail.com>
2023-11-10 13:23:37 +0530

    added user id to the model

--- backend/models/Workouts.js
+++ backend/models/Workouts.js
@@ -14,6 +14,10 @@ const workoutSchema = new Schema({
     load:{
         type: Number,
         required: true
+    },
+    user_id:{
+        type: String,
+        required: true
     }
     // timestamps gives us time of creation or updation.
 }, {timestamps:true});
